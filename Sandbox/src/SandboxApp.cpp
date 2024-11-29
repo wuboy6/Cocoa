70326b0a53855832b4a0f2b992fc6674edcf777b
wu_boy <wuboyonly@outlook.com>
2024-11-29 13:40:10 +0800

    debuging git

---
+++ Sandbox/src/SandboxApp.cpp
@@ -0,0 +1,47 @@
+#include <Cocoa.h>
+
+class ExampleLayer : public Cocoa::Layer
+{
+public:
+	ExampleLayer() :Layer("Example")
+	{
+
+	}
+
+	void OnUpdate() override
+	{
+		if (Cocoa::Input::IsKeyPressed(CO_KEY_TAB))
+			CO_TRACE("Tab key is pressed!");
+	}
+
+	void OnEvent(Cocoa::Event& event) override
+	{
+		if (event.GetEventType() == Cocoa::EventType::KeyPressed)
+		{
+			Cocoa::KeyPressedEvent& e = (Cocoa::KeyPressedEvent&)event;
+			CO_TRACE("{0}", (char)e.GetKeyCode());
+		}
+	}
+};
+
+class Sandbox : public Cocoa::Application
+{
+public:
+	Sandbox()
+	{
+		PushLayer(new ExampleLayer());
+		PushOverlay(new Cocoa::ImGuiLayer());
+	}
+
+	~Sandbox()
+	{
+		
+	}
+
+
+};
+
+Cocoa::Application* Cocoa::CreateApplication()
+{
+	return new Sandbox();
+}
