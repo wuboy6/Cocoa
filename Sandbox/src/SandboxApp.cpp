b12c177647c86f1c54c45cd6a188ef2aaae6fe4b
wu_boy <wuboyonly@outlook.com>
2024-12-05 22:30:55 +0800

    Transform

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -1,6 +1,8 @@
 #include <Cocoa.h>
 #include "imgui/imgui.h"
 
+#include <glm/gtc/matrix_transform.hpp>
+
 
 class ExampleLayer : public Cocoa::Layer
 {
@@ -41,10 +43,10 @@ public:
 		m_SquareVA.reset(Cocoa::VertexArray::Create());
 
 		float squareVertices[3 * 4] = {
-			-0.75f, -0.75f, 0.0f,
-			 0.75f, -0.75f, 0.0f,
-			 0.75f,  0.75f, 0.0f,
-			-0.75f,  0.75f, 0.0f
+			-0.5f, -0.5f, 0.0f,
+			 0.5f, -0.5f, 0.0f,
+			 0.5f,  0.5f, 0.0f,
+			-0.5f,  0.5f, 0.0f
 		};
 
 		std::shared_ptr<Cocoa::VertexBuffer> squareVB;
@@ -66,6 +68,7 @@ public:
 			layout(location = 1) in vec4 a_Color;
 
 			uniform mat4 u_ViewProjection;
+			uniform mat4 u_Transform;
 
 			out vec3 v_Position;
 			out vec4 v_Color;
@@ -74,7 +77,7 @@ public:
 			{
 				v_Position = a_Position;
 				v_Color = a_Color;
-				gl_Position =u_ViewProjection * vec4(a_Position , 1.0);
+				gl_Position =u_ViewProjection * u_Transform * vec4(a_Position , 1.0);
 			}
 		)";
 
@@ -101,13 +104,14 @@ public:
 			layout(location = 0) in vec3 a_Position;
 
 			uniform mat4 u_ViewProjection;
+			uniform mat4 u_Transform;
 
 			out vec3 v_Position;
 			
 			void main()
 			{
 				v_Position = a_Position;
-				gl_Position =u_ViewProjection * vec4(a_Position , 1.0);
+				gl_Position =u_ViewProjection * u_Transform * vec4(a_Position , 1.0);
 			}
 		)";
 
@@ -129,7 +133,6 @@ public:
 
 	void OnUpdate(Cocoa::Timestep ts) override
 	{
-		CO_TRACE("Delta time : {0}s ({1}ms)", ts.GetSeconds(),ts.GetMilliseconds());
 
 		if (Cocoa::Input::IsKeyPressed(CO_KEY_LEFT))
 			m_CameraPosition.x -= m_CameraMoveSpeed * ts;
@@ -154,7 +157,18 @@ public:
 
 		Cocoa::Renderer::BeginScene(m_Camera);
 
-		Cocoa::Renderer::Submit(m_BlueShader, m_SquareVA);
+		glm::mat4 scale = glm::scale(glm::mat4(1.0f), glm::vec3(0.1f));
+
+		for (int y = 0; y < 20; y++)
+		{
+			for (int x = 0; x < 20; x++)
+			{
+				glm::vec3 pos(x * 0.11f, y * 0.11f, 0.0f);
+				glm::mat4 transform = glm::translate(glm::mat4(1.0f), pos) * scale;
+				Cocoa::Renderer::Submit(m_BlueShader, m_SquareVA, transform);
+			}
+		}
+
 		Cocoa::Renderer::Submit(m_Shader, m_VertexArray);
 
 		Cocoa::Renderer::EndScene();
@@ -198,10 +212,10 @@ private:
 
 	Cocoa::OrthographicCamera m_Camera;
 	glm::vec3 m_CameraPosition;
-	float m_CameraMoveSpeed = 0.3f;
+	float m_CameraMoveSpeed = 1.0f;
 
 	float m_CameraRotation = 0.0f;
-	float m_CameraRotationSpeed = 15.0f;
+	float m_CameraRotationSpeed = 30.0f;
 };
 
 class Sandbox : public Cocoa::Application
